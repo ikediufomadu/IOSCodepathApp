84a8b793a9e1ff6300018944a8fe0967969bbee3
ikediufomadu <ikediufomadu@gmail.com>
2022-12-24 12:14:16 -0500

    QoL Changes

--- Codepath Entry/ViewController.swift
+++ Codepath Entry/ViewController.swift
@@ -22,20 +22,34 @@ class ViewController: UIViewController {
     }
     @IBAction func introduceSelfDidTap(_ sender: UIButton) {
         let year = yearSegmentControl.titleForSegment(at: yearSegmentControl.selectedSegmentIndex)
-        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) pets. " + petOrNoPet() + " I hope to see you on campus!"
-        //print(introduction)
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!.lowercased()) year and I own " + petOrPets() + petOrNoPet() + " I hope to see you on campus!"
         
         let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
         let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
         alertController.addAction(action)
         present(alertController, animated: true, completion: nil)
     }
+    
+    // Function to create a more grammatically correct sentence when talking about possiblity of more pets.
     func petOrNoPet() -> String {
         if (morePetsSwitch.isOn) {
-            return ("I plan on getting more pets!")
+            return "I plan on getting more pets!"
         }
         else {
-            return "I don't plan on getting anymore pets."
+            return "I do not plan on getting anymore pets."
+        }
+    }
+    
+    // Function to increase grammatical accuracy when talking about amount of pets.
+    func petOrPets() -> String {
+        if (numberOfPetsLabel.text == "1") {
+            return "a pet. "
+        }
+        else if (numberOfPetsLabel.text == "0") {
+            return "no pets. "
+        }
+        else {
+            return "\(numberOfPetsLabel.text!) pets. "
         }
     }
     override func viewDidLoad() {
