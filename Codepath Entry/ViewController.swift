474e3fee6ca1eec87847e5ac7ba52e4769abacce
ikediufomadu <ikediufomadu@gmail.com>
2022-12-24 11:10:42 -0500

    Finished

--- Codepath Entry/ViewController.swift
+++ Codepath Entry/ViewController.swift
@@ -9,6 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentControl: UISegmentedControl!
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    @IBAction func introduceSelfDidTap(_ sender: UIButton) {
+        let year = yearSegmentControl.titleForSegment(at: yearSegmentControl.selectedSegmentIndex)
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) pets. " + petOrNoPet() + " I hope to see you on campus!"
+        //print(introduction)
+        
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        alertController.addAction(action)
+        present(alertController, animated: true, completion: nil)
+    }
+    func petOrNoPet() -> String {
+        if (morePetsSwitch.isOn) {
+            return ("I plan on getting more pets!")
+        }
+        else {
+            return "I don't plan on getting anymore pets."
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
